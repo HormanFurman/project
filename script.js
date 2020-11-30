2d1c624abcbd69d6c4cdfe927eb1f1e4f3af4a96
hormanfurman <kievjoke@gmail.com>
2020-12-01 00:11:39 +0200

    puk

--- script.js
+++ script.js
@@ -42,9 +42,10 @@ const personalMovieDB = {
 
 for (let i = 0; i < 2; i++) {
 
-
     const a = prompt('Один из последних просмотренных фильмов?',''),
           b = prompt('На сколько оцените его?','');
+
+    if ()    
 
     personalMovieDB.movies[a] = b;
 
